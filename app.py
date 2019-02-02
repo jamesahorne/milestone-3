9b971e8a083d19b33595cb9e25810a0adf4be002
James Horne <james99h39@gmail.com>
2019-02-02 19:27:56 +0000

    Imported more Flask functions, Flask-PyMongo and ObjectId. Connected Flask to MongoDB. Added get_recipes route.

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'milestone_4'
+app.config['MONGO_URI'] = 'mongodb://#username#:#password#@ds251827.mlab.com:51827/milestone_4'
+
+
+# app.config['MONGO_URI'] = os.getenv('MONGO_URI') -   ! ! !   FIX THIS   ! ! !
+
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def test():
-    return 'Hello World'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
