2b488e5e63121d22f059bfe455fc62cf8186187c
James Horne <james99h39@gmail.com>
2019-02-27 10:18:29 +0000

    Added bar charts for vegetarian and time, and changed main ingredients to a pie chart

--- static/js/data.js
+++ static/js/data.js
@@ -6,25 +6,63 @@ function makeGraphs(error, recipeData) {
     var ndx = crossfilter(recipeData);
 
     main_ingredient(ndx);
+    vegetarian(ndx);
+    time(ndx);
     
     dc.renderAll();
 
 }
 
-function main_ingredients(ndx) {
-    var dim = ndx.dimension(dc.pluck('main_ingredient'));
+
+function main_ingredient(ndx) {
+  var dim = ndx.dimension(dc.pluck('main_ingredient'));
+  var group = dim.group();
+  
+  
+  dc.pieChart("#main_ingredient")
+      .height(400)
+      .width(350)
+      .radius(140)
+      .transitionDuration(1000)
+      .dimension(dim)
+      .group(group)
+      .legend(dc.legend());
+}
+
+
+function vegetarian(ndx) {
+    var dim = ndx.dimension(dc.pluck('vegetarian'));
     var group = dim.group();
 
-    dc.barChart("#main_ingredient")
-        .width(400)
-        .height(300)
+    dc.barChart("#vegetarian")
+        .width(500)
+        .height(350)
         .margins({ top: 10, right: 50, bottom: 50, left: 50 })
         .dimension(dim)
         .group(group)
-        .transitionDuration(500)
+        .transitionDuration(1000)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
-        .xAxisLabel("Main ingredients")
+        .xAxisLabel("Meat vs Vegetarian")
+        .yAxis().ticks(10);
+}
+
+
+function time(ndx) {
+    var dim = ndx.dimension(dc.pluck('time'));
+    var group = dim.group();
+
+    dc.barChart("#time")
+        .width(500)
+        .height(350)
+        .margins({ top: 10, right: 50, bottom: 50, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(1000)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Time")
         .yAxis().ticks(10);
 }
