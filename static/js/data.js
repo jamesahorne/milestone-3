6c0cc1aeb15a83fee43c7477c297a37b2521470d
James Horne <james99h39@gmail.com>
2019-03-11 15:45:19 +0000

    Add allergens pie chart and change 'time' bar chart to row chart

--- static/js/data.js
+++ static/js/data.js
@@ -2,31 +2,32 @@ queue()
     .defer(d3.json, "/get_data")
     .await(makeGraphs);
 
+
 function makeGraphs(error, recipeData) {
     var ndx = crossfilter(recipeData);
 
     main_ingredient(ndx);
     vegetarian(ndx);
     time(ndx);
-    
+    allergens(ndx);
+
     dc.renderAll();
 
 }
 
 
 function main_ingredient(ndx) {
-  var dim = ndx.dimension(dc.pluck('main_ingredient'));
-  var group = dim.group();
-  
-  
-  dc.pieChart("#main_ingredient")
-      .height(400)
-      .width(350)
-      .radius(140)
-      .transitionDuration(1000)
-      .dimension(dim)
-      .group(group)
-      .legend(dc.legend());
+    var dim = ndx.dimension(dc.pluck('main_ingredient'));
+    var group = dim.group();
+
+    dc.pieChart("#main_ingredient")
+        .width(350)
+        .height(500)
+        .radius(125)
+        .transitionDuration(1000)
+        .dimension(dim)
+        .group(group)
+        .legend(dc.legend());
 }
 
 
@@ -35,9 +36,9 @@ function vegetarian(ndx) {
     var group = dim.group();
 
     dc.barChart("#vegetarian")
-        .width(500)
-        .height(350)
-        .margins({ top: 10, right: 50, bottom: 50, left: 50 })
+        .width(350)
+        .height(500)
+        .margins({ top: 10, right: 50, bottom: 50, left: 30 })
         .dimension(dim)
         .group(group)
         .transitionDuration(1000)
@@ -52,17 +53,39 @@ function vegetarian(ndx) {
 function time(ndx) {
     var dim = ndx.dimension(dc.pluck('time'));
     var group = dim.group();
-
-    dc.barChart("#time")
-        .width(500)
-        .height(350)
-        .margins({ top: 10, right: 50, bottom: 50, left: 50 })
+    let scale = d3.scale.ordinal()
+        .domain([
+            "0-5 mins",
+            "5-10 mins",
+            "10-30 mins",
+            "30+ mins"
+        ])
+        .range([0, 1, 2, 3]);
+        
+    dc.rowChart("#time")
+        .width(350)
+        .height(500)
+        .margins({ top: 10, right: 20, bottom: 50, left: 30 })
         .dimension(dim)
         .group(group)
         .transitionDuration(1000)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .elasticY(true)
-        .xAxisLabel("Time")
-        .yAxis().ticks(10);
+        .elasticX(true)
+        .ordering(function(k) {
+            return scale(k.key);
+        });
+}
+
+
+function allergens(ndx) {
+    var dim = ndx.dimension(dc.pluck('allergens'));
+    var group = dim.group();
+
+    dc.pieChart("#allergens")
+        .width(350)
+        .height(500)
+        .radius(125)
+        .transitionDuration(1000)
+        .dimension(dim)
+        .group(group)
+        .legend(dc.legend());
 }
